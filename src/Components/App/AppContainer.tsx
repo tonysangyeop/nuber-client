8ebbc01f0cdd0ee8afa13485ee7dac4324066f9a
sanghae <sy_lee@wenwo.co.kr>
2019-01-17 22:38:50 +0900

    Typescript and Styled Components part Two

--- src/Components/App/AppContainer.tsx
+++ src/Components/App/AppContainer.tsx
@@ -1,8 +1,20 @@
 import React from "react";
 import { graphql } from "react-apollo";
+import reset from "styled-reset";
+import theme from '../../theme';
+import { createGlobalStyle, ThemeProvider } from '../../typed-components';
 import AppPresenter from "./AppPresenter";
 import { IS_LOGGED_IN } from "./AppQueries";
 
-const AppContainer = ({ data }) => <AppPresenter isLoggedIn={data.auth.isLoggedIn} />;
+// tslint:disable-next-line
+createGlobalStyle`
+   ${reset}   
+`
+
+const AppContainer = ({ data }) => (
+    <ThemeProvider theme={theme}>
+        <AppPresenter isLoggedIn={data.auth.isLoggedIn} />
+    </ThemeProvider>
+);
 
 export default graphql(IS_LOGGED_IN)(AppContainer); 
--- src/Components/App/AppPresenter.tsx
+++ src/Components/App/AppPresenter.tsx
@@ -5,7 +5,7 @@ interface IProps{
     isLoggedIn: boolean;
 }
 const AppPresenter: React.SFC<IProps> = ({ isLoggedIn }) => 
-    isLoggedIn ? <span>"you are in"</span> : <span>"you are out"</span>;
+    isLoggedIn ? <span>you are in</span> : <span>you are out</span>;
 
 AppPresenter.propTypes ={
     isLoggedIn: PropTypes.bool.isRequired
