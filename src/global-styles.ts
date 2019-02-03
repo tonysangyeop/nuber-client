31deefb9d4b2ece35a0a4b553651edb896a14056
sanghae <sy_lee@wenwo.co.kr>
2019-02-03 17:54:40 +0900

    OutHome Component

--- src/global-styles.ts
+++ src/global-styles.ts
@@ -4,19 +4,22 @@ import { createGlobalStyle } from './typed-components';
 // tslint:disable-next-line
 createGlobalStyle`
     @import url('https://fonts.googleapis.com/css?family=Maven+Pro');
-   ${reset}   
-   * {
-    box-sizing: border-box;
-   }
-   body{
-       font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
-   }
-   a{
-    color: inherit;
-    text-decoration: none;
-   }
-   input,
-   button{
-       &:focus,&:active{outline:none}
-   }
+    ${reset}   
+    * {
+        box-sizing: border-box;
+    }
+    body{
+        font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
+    }
+    a{
+        color: inherit;
+        text-decoration: none;
+    }
+    input,
+    button{
+        &:focus,&:active{outline:none}
+    }
+    h1,h2,h3,h4,h5,h6{
+      font-family:'Maven Pro', sans-serif;
+    }
 `;
